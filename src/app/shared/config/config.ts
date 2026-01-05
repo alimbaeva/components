3f0357653a8f2a8a14411d5e883ecf3079377233
asel <asel.alimbaeva.kk@gmail.com>
2026-01-05 20:12:55 +0300

    feat: add metadata

--- src/app/shared/config/config.ts
+++ src/app/shared/config/config.ts
@@ -1,6 +1,7 @@
 const IS_DEV = process.env.NODE_ENV === 'development'
-const TITLE = 'Компоненты на Next.js с использованием Tailwind CSS'
-const DESCRIPTION = 'Современных UI-компонентов на Next.js и Tailwind CSS'
+const TITLE = 'Компоненты на Next.js с использованием Tailwind CSS. Educational'
+const DESCRIPTION =
+  'Современных UI-компонентов на Next.js и Tailwind CSS. Educational'
 const APPLICATION_NAME = 'Компоненты для Next.js'
 const BASE_URL = IS_DEV
   ? 'http://localhost:3000/'

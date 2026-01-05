8aae73e75ae3c230b40dd82720f5b4d6a40c5ded
asel <asel.alimbaeva.kk@gmail.com>
2026-01-05 14:42:33 +0300

    feat: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,42 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import configPrettier from 'eslint-config-prettier'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      'sort-destructure-keys': sortDestructureKeys,
+    },
+    rules: {
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/ban-ts-comment': 'error',
+      'sort-destructure-keys/sort-destructure-keys': 'warn',
+      'react/destructuring-assignment': ['error', 'always'],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'react/jsx-sort-props': 'warn',
+      'react/jsx-curly-brace-presence': 'warn',
+      'import/no-unresolved': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  },
+  configPrettier,
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  compress: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig

2d634d33bbfd9831ed4dc0a74d3792cbde1db265
Asel <88249718+alimbaeva@users.noreply.github.com>
2026-01-06 15:42:27 +0300

    feat: add paragraph (#2)

---
+++ src/shared/ui/paragraph/paragraph.tsx
@@ -0,0 +1,42 @@
+import clsx from 'clsx'
+import type { Props } from './paragraph.props'
+
+const STANDARD_TYPE = {
+  medium: 'text-[16px] md:text-[20px] font-bold leading-[120%]',
+  semibold: 'text-[16px] md:text-[18px] font-semibold leading-[120%]',
+  normal: 'text-[14px] md:text-[18px] font-normal leading-[120%]',
+  minimal: 'text-[11px] md:text-[12px] leading-[100%]',
+  custom: '',
+}
+
+const COLOR = {
+  dark: 'text-neutral-100',
+  light: 'text-neutral-800',
+  custom: '',
+}
+
+const Paragraph = (props: Props) => {
+  const {
+    text,
+    type = 'normal',
+    color = 'dark',
+    variant = 'p',
+    href,
+    target,
+    className,
+  } = props
+
+  const Variant = variant
+
+  return (
+    <Variant
+      className={clsx(COLOR[color], STANDARD_TYPE[type], className)}
+      href={href}
+      target={target}
+    >
+      {text}
+    </Variant>
+  )
+}
+
+export { Paragraph }

18dc126652eb45742c446ad7d8167d8f5ffea186
asel <asel.alimbaeva.kk@gmail.com>
2026-01-06 18:47:39 +0300

    feat: add title

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,12 @@
+import { Title } from '@/ui/title/title'
+
 export default function Home() {
-  return <div>asel</div>
+  return (
+    <div>
+      <Title text='ppppppppp medium' />
+      <Title text='ppppppppp semibold' type='semibold' />
+      <Title text='ppppppppp normal' type='normal' />
+      <Title text='ppppppppp minimal' type='minimal' />
+    </div>
+  )
 }
---
+++ src/shared/ui/title/title.tsx
@@ -0,0 +1,45 @@
+import clsx from 'clsx'
+import type { Props } from './title.props'
+
+const STANDARD_TYPE = {
+  medium: 'text-[32px] md:text-[48px] font-bold leading-[110%] tracking-tight',
+  semibold:
+    'text-[24px] md:text-[36px] font-bold leading-[115%] tracking-tight',
+  normal: 'text-[20px] md:text-[28px] font-bold leading-[120%]',
+  minimal: 'text-[18px] md:text-[22px] font-semibold leading-[120%]',
+  custom: '',
+}
+
+const COLOR = {
+  dark: 'text-neutral-100',
+  light: 'text-neutral-800',
+  custom: '',
+}
+
+const Title = (props: Props) => {
+  const {
+    text,
+    variant = 'h2',
+    type = 'medium',
+    color = 'dark',
+    href,
+    target,
+    className,
+    children,
+  } = props
+
+  const Variant = variant
+
+  return (
+    <Variant
+      className={clsx(COLOR[color], STANDARD_TYPE[type], className)}
+      href={href}
+      target={target}
+    >
+      {text}
+      {variant === 'a' && <span>{children}</span>}
+    </Variant>
+  )
+}
+
+export { Title }

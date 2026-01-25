b93bc0f0ee6b5b9d9c454153e87c50eb5fa19dc1
Asel <88249718+alimbaeva@users.noreply.github.com>
2026-01-25 08:01:22 +0300

    feat: add InfiniteAutoplaySlider (#18)

--- src/views/slider-view/slider-view.tsx
+++ src/views/slider-view/slider-view.tsx
@@ -1,6 +1,7 @@
 import { BoundedControlledSlider } from '@/widgets/slider/embla-carousel/bounded-controlled-slider/bounded-controlled-slider'
 import { BoundedSlider } from '@/widgets/slider/embla-carousel/bounded-slider/bounded-slider'
 import { FullscreenCarousel } from '@/widgets/slider/embla-carousel/fullscreen-carousel/fullscreen-carousel'
+import { InfiniteAutoplaySlider } from '@/widgets/slider/embla-carousel/infinite-autoplay-slider/infinite-autoplay-slider'
 import { MultipleItemsSlider } from '@/widgets/slider/embla-carousel/multiple-items-slider/multiple-items-slider'
 import { ThumbnailsSlider } from '@/widgets/slider/embla-carousel/thumbnails-slider/thumbnails-slider'
 
@@ -12,6 +13,7 @@ const SliderView = () => {
       <BoundedControlledSlider />
       <ThumbnailsSlider />
       <MultipleItemsSlider />
+      <InfiniteAutoplaySlider />
     </div>
   )
 }

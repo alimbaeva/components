4f3bfdbd863a99fe667d937d2876857d774def3d
Asel <88249718+alimbaeva@users.noreply.github.com>
2026-01-28 18:36:43 +0300

    Feat/mobile (#23)
    * feat: add modal
    
    * feat: add animate

--- src/widgets/layout/header/header.tsx
+++ src/widgets/layout/header/header.tsx
@@ -5,23 +5,29 @@ import { Button } from '@/ui/button/button'
 import { Container } from '@/ui/container/container'
 import { useState } from 'react'
 import { Navigation } from '../navigation/navigation'
+import { useLockBodyScroll } from '@/hooks/use-lock-body-scroll'
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
   const handleClickMenu = () => setIsOpen((prevState) => !prevState)
+  useLockBodyScroll(isOpen)
   return (
-    <header className='relative block max-h-14 lg:hidden'>
+    <header className='fixed top-0 left-0 block max-h-14 w-full items-end bg-white'>
       <Container padding='minimal'>
         <Button
-          className='relative z-50 ml-auto size-10 outline-none'
+          className='relative z-50 ml-auto size-10 outline-none lg:hidden'
           onClick={handleClickMenu}
           size='custom'
         >
           <BurgerMenuBlock isOpen={isOpen} />
         </Button>
       </Container>
+      <Navigation isOpen={isOpen} onClick={handleClickMenu} />
       {isOpen && (
-        <Navigation className='absolute top-20 right-0 z-50 border-b lg:hidden' />
+        <div
+          className='fixed inset-0 top-14 z-50 bg-black/50'
+          onClick={() => setIsOpen(false)}
+        />
       )}
     </header>
   )
